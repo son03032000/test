2f3ab9af30e8a9fcd8307dfd51610b3458b652e2
Duong Duc Son <sndc0969755036@gmail.com>
2020-07-09 08:48:22 +0700

    1

---
+++ test.js
@@ -0,0 +1,20 @@
+function bp(x){
+    console.log(x*x)
+    }
+bp(6);
+
+ function cv(x,y){
+     console.log(2*(x+y));
+        }
+cv(5,6);
+
+function cc(n){
+    for(i = 0; i< 10; i++){
+        kq = n*i;
+        console.log(` ${n} * ${i} = ${kq} `);
+    }
+               }
+cc(3);
+cc(4);
+bp(7);
+
